3911a1383c6d57c926cc33a600ba3faa0df63012
lihelennn <helen82198@gmail.com>
2016-03-03 01:28:25 -0500

    hw04 svg not quite working yet

--- hw04/interactive.js
+++ hw04/interactive.js
@@ -2,33 +2,40 @@ var pic = document.getElementById("vimage");
 
 //this.setAttribute("fill","green");
 
-var c = document.createElementNS("https://www.w3.org/2000/svg","circle");
+var mySVG = document.createElementNS("https://www.w3.org/2000/svg","svg");
+mySVG.setAttribute("height",500);
+mySVG.setAttribute("width",500);
+mySVG.setAttribute("id","vimage");
+mySVG.setAttribute("style","border:2px solid;");
+document.body.appendChild(mySVG);
 
-var circle = document.getElementByTagName("circle")[0];
-var radius = parseInt(c.getAttribute("r"));
-var width = pic.width;
-var height = pic.height;
+var radius = 100;
+var width = mySVG.getAttribute("width");
+var height = mySVG.getAttribute("height");
 var expanding = true;
 
+//var start = document.getElementById("circle");
+//var stop = document.getElementById("stop");
+//var dvd = document.getElementById("dvd");
+mySVG.addEventListener("click",clicked);
+//start.addEventListener("click", changeSize);
 
+function clear(){
+    var c = document.createElementNS("https://www.w3.org/2000/svg","circle");
+    c.setAttribute("cx",0);
+    c.setAttribute("cy",0);
+    c.setAttribute("r",radius.toString());
+    c.setAttribute("fill","white");
+    c.setAttribute("stroke","white");
+    mySVG.appendChild(c);
+}
 
-c.setAttribute("cx",0);
-c.setAttribute("cy",0);
-c.setAttribute("r",radius.toString());
-c.setAttribute("fill","red");
-c.setAttribute("stroke","black");
-pic.appendChild(c);
-
-var start = document.getElementById("circle");
-var stop = document.getElementById("stop");
-var dvd = document.getElemenyById("dvd");
-start.addEventListener("click", changeSize);
-
-var changeSize = function(e){
+var changeSize = function(){
+    console.log(radius);
     if (radius == width/2){
 	expanding = false;
     }else{
-	if ( radius = 0){
+	if (radius == 0){
 	    expanding = true;
         }
     }
@@ -40,15 +47,27 @@ var changeSize = function(e){
     drawDot();
 }
 
+function drawDot(){
+    var c = document.createElementNS("https://www.w3.org/2000/svg","circle");
+    console.log("hello");
+    clear();
+    c.setAttribute("cx",0);
+    c.setAttribute("cy",0);
+    c.setAttribute("r",radius.toString());
+    c.setAttribute("fill","red");
+    c.setAttribute("stroke","black");
+    mySVG.appendChild(c);
+    console.log(document.getElementsByTagName("svg"));
 
-
-var clicked = function(e){
-    if(e.toElement == this){
-	drawDot(e.offsetX,e.offsetY);
-    }
 }
 
-pic.addEventListener("click",clicked);
+function clicked(e){
+    //console.log(radius);
+    changeSize();
+    intervalID = window.setInterval(clicked,16);
 
-intervalID = window.setInterval(<fxn>,16);
+}
+
+//drawDot(e.offsetX,e.offsetY);
+
 
